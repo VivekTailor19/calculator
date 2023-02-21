119ef4737cd35be25b5dbff3d393503c5dbc0c63
Vivek <tailorvivekgec19@gmail.com>
2023-02-21 03:21:45 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:calculaor/UI_Calculator/ui_calculator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: UI_Calculaor(),
+  ));
+}
